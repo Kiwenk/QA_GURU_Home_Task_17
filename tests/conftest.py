250286c1fd8deb549a74e23d7e36f8b2b8a75887
Katsuba Igor <kasubaigor@gmail.com>
2024-08-10 21:25:28 +0300

    Исправления

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,42 @@ import pytest
 
 
 @pytest.fixture()
-def path_list_users():
-    path_list_users_str = str(os.path.abspath('../schema/list_users.json'))
-    print(path_list_users_str)
-    return path_list_users_str
+def path_list_users_schema():
+    path_list_users_schema_str = str(os.path.abspath('../schema/list_users.json'))
+    return path_list_users_schema_str
 
 
+@pytest.fixture()
+def path_create_user_schema():
+    path_create_user_schema_str = str(os.path.abspath('../schema/create_user.json'))
+    return path_create_user_schema_str
 
-    # path_create_user = str(os.path.abspath('schema/create_user.json'))
-    #
-    # path_single_users = str(os.path.abspath('schema/single_users.json'))
-    # path_update_user = str(os.path.abspath('schema/update_user.json'))
 
+@pytest.fixture()
+def path_single_users_schema():
+    path_single_users_schema_str = str(os.path.abspath('../schema/single_user.json'))
+    return path_single_users_schema_str
+
+
+@pytest.fixture()
+def path_update_user_schema():
+    path_update_user_schema_str = str(os.path.abspath('../schema/update_user.json'))
+    return path_update_user_schema_str
+
+
+@pytest.fixture()
+def path_create_user():
+    path_create_user_str = str(os.path.abspath('../resources/create_user.json'))
+    return path_create_user_str
+
+
+@pytest.fixture()
+def path_update_user():
+    path_update_user_str = str(os.path.abspath('../resources/update_user.json'))
+    return path_update_user_str
+
+
+@pytest.fixture()
+def path_update_user_failed():
+    update_user_failed_str = str(os.path.abspath('../resources/update_user_failed.json'))
+    return update_user_failed_str
--- tests/test_api_regres.py
+++ tests/test_api_regres.py
@@ -3,9 +3,9 @@ import json
 from jsonschema import validate
 
 
-def test_list_users(path_list_users):
+def test_list_users(path_list_users_schema):
     response = requests.get(url='https://reqres.in/api/users', params={'page': 2})
-    with open(f'{path_list_users}') as file:
+    with open(f'{path_list_users_schema}') as file:
         data = json.load(file)
     body = response.json()
     validate(body, data)
@@ -17,39 +17,39 @@ def test_list_users_negative():
     assert response.status_code != 201
 
 
-def test_create_user():
-    with open('../resources/create_user.json') as file:
+def test_create_user(path_create_user, path_create_user_schema):
+    with open(f'{path_create_user}') as file:
         data_in = json.load(file)
     response = requests.post(url='https://reqres.in/api/users', json=data_in)
-    with open("../schema/create_user.json") as validate_file:
+    with open(f'{path_create_user_schema}') as validate_file:
         data_validate = json.load(validate_file)
     body = response.json()
     validate(body, data_validate)
     assert response.status_code == 201
 
 
-def test_update_user():
-    with open('../resources/update_user.json') as file:
+def test_update_user(path_update_user, path_update_user_schema):
+    with open(f'{path_update_user}') as file:
         data = json.load(file)
     response = requests.put(url='https://reqres.in/api/users/2', json=data)
-    with open("../schema/update_user.json") as validate_file:
+    with open(f"{path_update_user_schema}") as validate_file:
         data_validate = json.load(validate_file)
     body = response.json()
     validate(body, data_validate)
     assert response.status_code == 200
 
 
-def test_single_users_validate():
+def test_single_users_validate(path_single_users_schema):
     response = requests.get(url='https://reqres.in/api/users/2')
-    with open('../schema/single_user.json') as file:
+    with open(f'{path_single_users_schema}') as file:
         data = json.load(file)
     body = response.json()
     validate(body, data)
     assert response.status_code == 200
 
 
-def test_update_user_failed():
-    with open('../resources/update_user_failed.json') as file:
+def test_update_user_failed(path_update_user_failed):
+    with open(f'{path_update_user_failed}') as file:
         data = file.read()
     response = requests.put(url='https://reqres.in/api/users/2', json=data)
     assert response.status_code == 400
