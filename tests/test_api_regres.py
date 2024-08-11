f4cf27eb51f7d640f9a24b258143189e7e52b4ad
Katsuba Igor <kasubaigor@gmail.com>
2024-08-11 11:26:18 +0300

    Исправления абсолютного пути файлов

--- tests/test_api_regres.py
+++ tests/test_api_regres.py
@@ -1,14 +1,15 @@
 import requests
 import json
 from jsonschema import validate
+from path import path_resources, path_schemas
 
 
-def test_list_users(path_list_users_schema):
+def test_list_users():
     response = requests.get(url='https://reqres.in/api/users', params={'page': 2})
-    with open(f'{path_list_users_schema}') as file:
-        data = json.load(file)
+    with open(f'{path_schemas('list_users.json')}') as file:
+        data_validate = json.load(file)
     body = response.json()
-    validate(body, data)
+    validate(body, data_validate)
     assert response.status_code == 200
 
 
@@ -17,39 +18,39 @@ def test_list_users_negative():
     assert response.status_code != 201
 
 
-def test_create_user(path_create_user, path_create_user_schema):
-    with open(f'{path_create_user}') as file:
+def test_create_user():
+    with open(f'{path_resources('create_user.json')}') as file:
         data_in = json.load(file)
     response = requests.post(url='https://reqres.in/api/users', json=data_in)
-    with open(f'{path_create_user_schema}') as validate_file:
+    with open(f'{path_schemas('create_user.json')}') as validate_file:
         data_validate = json.load(validate_file)
     body = response.json()
     validate(body, data_validate)
     assert response.status_code == 201
 
 
-def test_update_user(path_update_user, path_update_user_schema):
-    with open(f'{path_update_user}') as file:
+def test_update_user():
+    with open(f'{path_resources('update_user.json')}') as file:
         data = json.load(file)
     response = requests.put(url='https://reqres.in/api/users/2', json=data)
-    with open(f"{path_update_user_schema}") as validate_file:
+    with open(f"{path_schemas('update_user.json')}") as validate_file:
         data_validate = json.load(validate_file)
     body = response.json()
     validate(body, data_validate)
     assert response.status_code == 200
 
 
-def test_single_users_validate(path_single_users_schema):
+def test_single_user_validate():
     response = requests.get(url='https://reqres.in/api/users/2')
-    with open(f'{path_single_users_schema}') as file:
+    with open(f'{path_schemas('single_user.json')}') as file:
         data = json.load(file)
     body = response.json()
     validate(body, data)
     assert response.status_code == 200
 
 
-def test_update_user_failed(path_update_user_failed):
-    with open(f'{path_update_user_failed}') as file:
+def test_update_user_failed():
+    with open(f'{path_resources('update_user_failed.json')}') as file:
         data = file.read()
     response = requests.put(url='https://reqres.in/api/users/2', json=data)
     assert response.status_code == 400
